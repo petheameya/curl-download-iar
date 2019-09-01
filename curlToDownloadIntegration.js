3f849704719a938118732183456d4493a44c0120
Ameya Pethe <ameya.pethe@sherwin.com>
2019-09-01 13:22:01 -0400

    added script curlToDownloadIntegration.js with place holder for credentials to be inserted by jenkins at runtime.

---
+++ curlToDownloadIntegration.js
@@ -0,0 +1,98 @@
+//const { Curl } = require('node-libcurl');
+const { Curl, CurlFeature } = require('node-libcurl');
+const fs = require('fs');
+
+const fileOutPath = './exports/LOADPDHEMEABULKUPDATESBETA_01.00.0000.iar';
+
+const fileOut = fs.openSync(fileOutPath, 'w+')
+
+const curlDownload = new Curl();
+
+curlDownload.setOpt(Curl.option.URL, 'https://swoicdev1-sherwinwilliamsoci.integration.ocp.oraclecloud.com:443/ic/api/integration/v1/integrations/LOADPDHEMEABULKUPDATESBETA|01.00.0000/archive');
+//curlDownload.setOpt(Curl.option.USERNAME, '');
+curlDownload.setOpt(Curl.option.USERPWD, process.argv[2] + ':' + process.argv[3]);
+curlDownload.setOpt(Curl.option.SSL_VERIFYHOST, 0);
+curlDownload.setOpt(Curl.option.SSL_VERIFYPEER, 0);
+//curlDownload.setOpt(Curl.option.VERBOSE, true);
+
+curlDownload.setOpt(Curl.option.WRITEFUNCTION, (buff, nmemb, size) => {
+    let written = 0
+  
+    if (fileOut) {
+      written = fs.writeSync(fileOut, buff, 0, nmemb * size)
+    } else {
+      /* listing output */
+      process.stdout.write(buff.toString())
+      written = size * nmemb
+    }
+  
+    return written
+  })
+
+curlDownload.enable(CurlFeature.Raw | CurlFeature.NoStorage)
+
+curlDownload.on('end', function(statusCode, data, headers){
+    //console.log('End');
+    //console.log(statusCode);
+    //console.log(data);
+    //console.log(headers);
+
+    fs.closeSync(fileOut);
+
+    this.close();
+
+    curlDownload.close.bind(curlDownload);
+});
+
+curlDownload.on('error', function(error, errorCode){
+    console.log('Error');
+    console.log(error);
+    console.log(errorCode);
+    fs.closeSync(fileOut);
+    curlDownload.close();
+});
+
+curlDownload.perform();
+
+/*
+    Now start uploading file that we downloaded
+*/
+
+const curlUpload = new Curl();
+
+const data = [{
+        name: 'fileName',
+        contents: 'IAR File',
+    },
+    {
+    name: 'file',
+    file: 'fileOutPath',
+}];
+
+curlUpload.setOpt(Curl.option.URL, 'https://swoicqa1-sherwinwilliamsoci.integration.ocp.oraclecloud.com:443/ic/api/integration/v1/integrations/archive');
+//curlDownload.setOpt(curlDownload.option.USERNAME, '');
+curlUpload.setOpt(Curl.option.USERPWD, 'Geaintsoadev1:SellmorepaintSOA18*');
+curlUpload.setOpt(Curl.option.SSL_VERIFYHOST, 0);
+curlUpload.setOpt(Curl.option.HTTPPOST, data);
+curlUpload.setOpt(Curl.option.SSL_VERIFYPEER, 0);
+curlUpload.setOpt(Curl.option.VERBOSE, true);
+
+curlUpload.on('end', function(statusCode, body){
+    console.log('Response from upload-');
+    console.log({
+        statusCode,
+        body: JSON.parse(body)
+    });
+
+    this.close();
+});
+
+curlUpload.on('error', function(error, errorCode){
+    console.error(error, errorCode);
+    this.close();
+});
+
+curlUpload.perform();
+
+
+
